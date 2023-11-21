25d9c6fb75b4e1bf1f90a231311e8d9f8770e813
Liza De Guerengomba <LDeGuereng@my.gcu.edu>
2023-11-21 15:22:38 -0700

    the start of the project, dependancies, and etc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Box} from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <BrowserRouter>
+        <Box sx = {{ backgroundColor: '#000'}}>
+            NavBar
+            {/* built in component */}
+            <Routes> 
+                {/* individual route components */}
+                {/* root route and has an elemnt that is giong to render a feed component */}
+                <Route path="/" exact element={<Feed/>} />
+                {/* is a specfic video and its details */}
+                <Route path="/video/:id" element={<VideoDetail/>} />
+                {/* is a specfic channel */}
+                <Route path="/channel/:id" element={<ChannelDetail/>} />
+                {/* search route */}
+                <Route path="/search/:searcTerm" element={<SearchFeed/>} />
+                
+            </Routes>
+        </Box>
+    </BrowserRouter>
+  )
 
-export default App;
+export default App
